c5eee08cb48e8b114cfa7eb352fb088e34f9ef55
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-09-20 17:25:13 +0530

    Update MySkillsPage.js

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React,{useCallback}  from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { lightTheme } from './Themes';
 import { Design, Develope } from './AllSvgs';
