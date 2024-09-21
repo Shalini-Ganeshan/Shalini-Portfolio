200c4011df71d95862cd71d82bea6e6434e941f4
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-09-21 11:08:17 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import React from 'react';
+import React,{useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
 import LogoComponent from '../subComponents/LogoComponent';
