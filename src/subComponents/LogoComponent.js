b508c69cf031da72f50013d86d2a7ce38641014a
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-09-26 18:27:09 +0530

    Update LogoComponent.js

--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -1,27 +1,38 @@
-import React from 'react'
-import styled from 'styled-components'
-import { DarkTheme } from '../components/Themes'
-
-
-
+import React from 'react';
+import styled from 'styled-components';
+import { DarkTheme } from '../components/Themes';
 
 const Logo = styled.h1`
-display: inline-block;
-color: ${props => props.color === 'dark' ? DarkTheme.text : DarkTheme.body};
-font-family: 'Pacifico',cursive;
+  display: inline-block;
+  color: ${(props) => (props.color === 'dark' ? DarkTheme.text : DarkTheme.body)};
+  font-family: 'Pacifico', cursive;
+  position: fixed;
+  left: 2rem;
+  top: 2rem;
+  z-index: 3;
+  font-size: 2.5rem; /* Default font size for large screens */
 
-position: fixed;
-left: 2rem;
-top: 2rem;
-z-index:3;
-`
+  /* For medium screen sizes (tablets, landscape) */
+  @media (max-width: 1024px) {
+    font-size: 2rem; /* Decrease font size */
+    left: 1.5rem;
+    top: 1.5rem;
+  }
+
+  /* For small screen sizes (mobile devices) */
+  @media (max-width: 768px) {
+    font-size: 1.5rem; /* Further decrease font size */
+    left: 1rem;
+    top: 1rem;
+  }
+`;
 
 const LogoComponent = (props) => {
-    return (
-        <Logo color={props.theme}>
-          SG
-        </Logo>
-    )
-}
+  return (
+    <Logo color={props.theme}>
+      SG
+    </Logo>
+  );
+};
 
-export default LogoComponent
+export default LogoComponent;
