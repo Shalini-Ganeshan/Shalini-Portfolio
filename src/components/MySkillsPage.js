72a38f139b0f1250d5b81e9763bd785cd686ac86
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-09-26 19:45:22 +0530

    Update MySkillsPage.js

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -21,15 +21,7 @@ const Box = styled.div`
    @media (max-width: 768px) {
     flex-direction: column; 
   }
-    @media (max-width: 768px) {
-    width: 80vw; /* Adjust width for medium screens */
-    height: auto; /* Allow height to adjust based on content */
-    margin: 1rem 0; /* Add margin for spacing */
-  }
-
-  @media (max-width: 480px) {
-    width: 90vw; /* Adjust width for small screens */
-  }
+ 
 `;
 const Main = styled(motion.div)`
   border: 2px solid ${props => props.theme.text};
@@ -47,13 +39,13 @@ const Main = styled(motion.div)`
   justify-content: space-between;
 
   @media (max-width: 768px) {
-    width: 80vw; /* Adjust width for medium screens */
+    width: 70vw; /* Adjust width for medium screens */
     height: auto; /* Allow height to adjust based on content */
     margin: 1rem 0; /* Add margin for spacing */
   }
 
   @media (max-width: 480px) {
-    width: 90vw; /* Adjust width for small screens */
+    width: 60vw; /* Adjust width for small screens */
   }
 
   background: rgba(0, 0, 0, 0.0);
