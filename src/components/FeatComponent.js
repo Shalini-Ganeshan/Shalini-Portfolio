28292a0da4ffb6b5c62356e19ed4e886d64b4726
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-09-26 18:09:07 +0530

    Update FeatComponent.js

--- src/components/FeatComponent.js
+++ src/components/FeatComponent.js
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import React from "react";
-// import { NavLink } from 'react-router-dom'
 import styled from "styled-components";
 
 const Box = styled(motion.a)`
@@ -13,7 +12,7 @@ const Box = styled(motion.a)`
   backdrop-filter: blur(2px);
   box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
   cursor: pointer;
-position:relative;
+  position: relative;
   display: flex;
   flex-direction: column;
   z-index: 5;
@@ -23,24 +22,22 @@ position:relative;
     background-color: ${(props) => props.theme.text};
     transition: all 0.3s ease;
   }
-    @media (max-width: 1024px) {  /* Medium screen sizes */
-    width: calc(12rem + 20vw);  /* Increase width */
+
+  @media (max-width: 1024px) {
+    width: calc(12rem + 20vw);
   }
 
-  @media (max-width: 768px) {  /* Small screen sizes */
-        width: calc(10rem + 20vw);
+  @media (max-width: 768px) {
+    width: calc(10rem + 20vw);
   }
 `;
 
-const Image = styled.div`
-  background-image: ${(props) => `url(${props.img})`};
-  background-size: contain; /* Ensures the entire image is visible */
-  background-repeat: no-repeat; /* Prevents repeating of the image */
-  background-position: center center; /* Centers the image within the container */
+const Image = styled.img`
   width: 100%;
-  height: 60%; /* Adjust the height as needed */
-  border: 1px solid transparent; /* Initially transparent border */
-  
+  height: 60%;
+  object-fit: contain; /* Ensures the entire image is visible */
+  border: 1px solid transparent;
+
   ${Box}:hover & {
     border: 1px solid ${(props) => props.theme.body}; /* Border color on hover */
   }
@@ -53,17 +50,21 @@ const Title = styled.h3`
   font-family: "Karla", sans-serif;
   font-weight: 700;
   border-bottom: 1px solid ${(props) => props.theme.text};
-text-align:center;
+  text-align: center;
+
   ${Box}:hover & {
     border-bottom: 1px solid ${(props) => props.theme.body};
   }
 `;
+
 const HashTags = styled.div`
   padding: 0.5rem 0;
 `;
+
 const Tag = styled.span`
   padding-right: 0.5rem;
 `;
+
 const Date = styled.span`
   padding: 0.5rem 0;
 `;
@@ -87,23 +88,24 @@ const Item = {
 const FeatComponent = (props) => {
   const { name, tags, date, imgSrc, link } = props.blog;
   return (
-    <motion.div    initial={{ opacity: 0 }} 
-    animate={{opacity:1}}
-    transition={{ duration: 0.1, ease: "easeIn"}} 
- 
-    > 
-    <Container variants={Item}>
-      <Box target="_blank" href={`${link}`}>
-        <Image img={imgSrc} />
-        <Title>{name}</Title>
-        <HashTags>
-          {tags.map((t, id) => {
-            return <Tag key={id}>#{t}</Tag>;
-          })}
-        </HashTags>
-        <Date>{date}</Date>
-      </Box>
-    </Container>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.1, ease: "easeIn" }}
+    >
+      <Container variants={Item}>
+        <Box target="_blank" href={`${link}`}>
+          {/* Lazy loaded image */}
+          <Image src={imgSrc} alt={name} loading="lazy" />
+          <Title>{name}</Title>
+          <HashTags>
+            {tags.map((t, id) => (
+              <Tag key={id}>#{t}</Tag>
+            ))}
+          </HashTags>
+          <Date>{date}</Date>
+        </Box>
+      </Container>
     </motion.div>
   );
 };
