dec743c1d72b2d5091856ea9a7308efdaea4d427
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-09-27 22:46:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme } from "./components/Themes";
 import { AnimatePresence } from "framer-motion";
 import GlobalStyle from "./globalStyles";
-import Loading from "./subComponents/Loading"; // Import Loading component
+import Loading from "./subComponents/Loading"; 
 
-// Components
+
 import Main from "./components/Main";
 import AboutPage from "./components/AboutPage";
 import Feats from "./components/Feats";
@@ -18,22 +18,22 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    // Set loading to true when the page is refreshed
+    
     const handleBeforeUnload = () => {
-      setIsLoading(true); // Show loading when the page is refreshing
+      setIsLoading(true); 
     };
 
-    // Add event listener for beforeunload
+    
     window.addEventListener("beforeunload", handleBeforeUnload);
 
-    // Cleanup the event listener on unmount
+   
     return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
     };
   }, []);
 
   useEffect(() => {
-    // Hide loading after the component mounts
+   
     setIsLoading(false);
   }, [location]);
 
@@ -41,10 +41,7 @@ function App() {
     <>
       <GlobalStyle />
       <ThemeProvider theme={lightTheme}>
-        {/* Show loading when the app is loading */}
         {isLoading && <Loading />}
-
-        {/* For framer-motion animation on page change! */}
         <AnimatePresence mode='wait'>
           <Routes key={location.pathname} location={location}>
             <Route path="/" element={<Main />} />
