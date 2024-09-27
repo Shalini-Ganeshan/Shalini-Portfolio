6bc39f2a97cd4ab1ffc13e6094d02dab1b0ec8ce
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-09-27 22:06:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
+import React, { useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { lightTheme } from "./components/Themes";
 import { AnimatePresence } from "framer-motion";
 import GlobalStyle from "./globalStyles";
+import Loading from "./subComponents/Loading"; // Import Loading component
 
-//Components
+// Components
 import Main from "./components/Main";
 import AboutPage from "./components/AboutPage";
 import Feats from "./components/Feats";
 import WorkPage from "./components/WorkPage";
 import MySkillsPage from "./components/MySkillsPage";
 
-
 function App() {
   const location = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    // Set loading to true when the page is refreshed
+    const handleBeforeUnload = () => {
+      setIsLoading(true); // Show loading when the page is refreshing
+    };
+
+    // Add event listener for beforeunload
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    // Cleanup the event listener on unmount
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Hide loading after the component mounts
+    setIsLoading(false);
+  }, [location]);
+
   return (
     <>
       <GlobalStyle />
-
       <ThemeProvider theme={lightTheme}>
-      
+        {/* Show loading when the app is loading */}
+        {isLoading && <Loading />}
 
         {/* For framer-motion animation on page change! */}
-        {/* Changed prop from exitBefore to mode */}
         <AnimatePresence mode='wait'>
-          {/* Changed Switch to Routes */}
-
-          <Routes key={location.pathname} location={location} >
-            {/* Changed component to element */}
-
+          <Routes key={location.pathname} location={location}>
             <Route path="/" element={<Main />} />
-
             <Route path="/about" element={<AboutPage />} />
-
             <Route path="/feats" element={<Feats />} />
-
             <Route path="/work" element={<WorkPage />} />
-
             <Route path="/skills" element={<MySkillsPage />} />
-            {/* Below is to catch all the other routes and send the user to main component,
-you can add custom 404 component or message instead of Main component*/}
             <Route path="*" element={<Main />} />
           </Routes>
         </AnimatePresence>
