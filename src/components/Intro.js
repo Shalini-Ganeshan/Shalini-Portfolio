ae155945a5007850d03e63f506f8b3082bad8cdc
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-10-02 02:12:30 +0530

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,26 +8,18 @@ position: absolute;
 left: 50%;
 top: 50%;
 transform: translate(-50%, -50%);
+
+
 width: 65vw;
 height:55vh;
 display: flex;
+
+
 background: linear-gradient(
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  width: 65vw;
-  height: 55vh;
-  display: flex;
-  background: linear-gradient(
     to right,
     ${props => props.theme.body} 50%,
     ${props => props.theme.text} 50%) bottom,
     linear-gradient(
-    ${(props) => props.theme.body} 50%,
-    ${(props) => props.theme.text} 50%
-  ) bottom,
-  linear-gradient(
     to right,
     ${props => props.theme.body} 50%,
     ${props => props.theme.text} 50%) top;
@@ -35,15 +27,10 @@ background: linear-gradient(
 background-size: 100% 2px;
     border-left: 2px solid ${props => props.theme.body};
     border-right: 2px solid ${props => props.theme.text};
+
+
     z-index:1;
-    ${(props) => props.theme.body} 50%,
-    ${(props) => props.theme.text } 50%
-  ) top;
-  background-repeat: no-repeat;
-  background-size: 100% 2px;
-  border-left: 2px solid ${(props) => props.theme.body};
-  border-right: 2px solid ${(props) => props.theme.text};
-  z-index: 1;
+
   @media (max-width: 768px) {
     flex-direction: column;
     width: 51vw;
