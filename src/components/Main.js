37955ab6167cce43bde68800dcb513e484b10569
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-10-06 09:04:52 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -80,6 +80,7 @@ const BLOG = styled(NavLink)`
     right: calc(0.4rem + 2vw); 
     font-size:1rem;
     color: ${props => props.click ? props.theme.body : props.theme.text};
+     text-shadow: ${props => props.click ? '1px 1px 2px black, 0 0 25px black, 0 0 5px black' : 'none'};
   }
   @media (max-width: 500px) {
     right: calc(0.4rem + 2vw); 
