7d5132dcfe059db433506ae23f742499a476dcec
Shalini Ganeshan <140951031+Shalini-Ganeshan@users.noreply.github.com>
2024-11-01 20:49:06 +0530

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -50,7 +50,7 @@ export const Work = [
         name: "Buzz Hub App",
         description: "A Real-time Messenger application enabling seamless instant messaging and collaboration.",
         tags: ["graphql","apollo-server","web-sockets","nextjs"],
-        demo: "https://drive.google.com/file/d/1L_uBfZ2dY26ZtaOViASpSHRTVYaKPiR3/view?usp=sharing",
+        demo: "https://drive.google.com/file/d/1GbR4KFY-92LWsDrpWtU8a6okm2Z7-SBZ/view?usp=sharing",
         github: "https://github.com/Shalini-Ganeshan/BuzzHub-ChatApp",
         image: buzzHub
     },
